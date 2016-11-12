487117d7b2b1bc7e7d2a156926a1353fb38ffefc
Tim-P <TimPelzl@gmx.de>
2016-11-12 13:30:31 +0100

    Added NuGet-Package: Azure.Devices

---
+++ HackaTUM/HackaTUM/Classes/AzureConnectionHandler.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+
+
+namespace HackaTUM.Classes
+{
+    class AzureConnectionHandler
+    {
+        //--------------------------------------------------------Atribute:-------------------------------------------------------------------\\
+        #region --Atribute--
+
+        private DeviceClient deviceClient;
+        private string iotHubHostname = "{iot hub hostname}";
+        private string deviceId = "{device id}";
+        private string deviceKey = "{device key}";
+
+
+        #endregion
+        //--------------------------------------------------------Construktoren:--------------------------------------------------------------\\
+        #region --Construktoren--
+
+
+        #endregion
+        //--------------------------------------------------------Set-, Get- Metoden:---------------------------------------------------------\\
+        #region --Set-, Get- Metode--
+
+
+
+        #endregion
+        //--------------------------------------------------------Sonstige Metoden:-----------------------------------------------------------\\
+        #region --Sonstige Metoden (Public)--
+
+
+        
+        public static void initialize()
+        {
+            // Initialize device client
+            deviceClient = DeviceClient.Create(iotHubHostname,
+                new DeviceAuthenticationWithRegistrySymmetricKey(deviceId, deviceKey));
+        }
+
+
+        #endregion
+
+        #region --Sonstige Metoden (Private)--
+
+
+
+        #endregion
+
+        #region --Sonstige Metoden (Protected)--
+
+
+
+        #endregion
+        //--------------------------------------------------------Events:---------------------------------------------------------------------\\
+        #region --Events--
+
+
+
+        #endregion
+
+
+    }
+}
