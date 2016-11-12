8807d8857206f79ffaacf7e2f3336ab1326c335e
KingKili <kilian.traub@mailbox.org>
2016-11-12 13:22:18 +0100

    Completing Google Maps integration

--- HackaTUM/HackaTUM/Classes/TravelDataManager.cs
+++ HackaTUM/HackaTUM/Classes/TravelDataManager.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net.Http;
-using System.Text.RegularExpressions;
 
 namespace HackaTUM.Classes
 {
@@ -43,16 +42,16 @@ namespace HackaTUM.Classes
 
         #endregion
 
-        public static String getConnectionInfo()
+        public static String getConnectionInfo(DateTime DestinationTime)
         {
-           return(getConnectionInfo(DataStorage.INSTANCE.userData.adressHome, DataStorage.INSTANCE.userData.adressWork));
+           return(getConnectionInfo(DataStorage.INSTANCE.userData.adressHome, DataStorage.INSTANCE.userData.adressWork, DestinationTime));
         }
-        public static String getConnectionInfo(String startAdress, String Destination)
+        public static String getConnectionInfo(String startAdress, String Destination, DateTime DestinationTime)
         {
             //DataStorage.INSTANCE.userData.adressHome
             using (var client = new HttpClient())
             {
-                var response = client.GetAsync("https://maps.googleapis.com/maps/api/distancematrix/xml?origins=" + startAdress + "&destinations=" + Destination + "&mode=transit&key=AIzaSyCrBZT_RgE-4Pks55IG3dOceTq4pyUQGfo").Result;
+                var response = client.GetAsync("https://maps.googleapis.com/maps/api/distancematrix/xml?origins=" + startAdress + "&destinations=" + Destination + "&mode=transit&arrival_time="+Convert.ToString((DateTime.UtcNow - DestinationTime).TotalMilliseconds)+"&key=AIzaSyCrBZT_RgE-4Pks55IG3dOceTq4pyUQGfo").Result;
 
                 if (response.IsSuccessStatusCode)
                 {
@@ -68,24 +67,66 @@ namespace HackaTUM.Classes
                 }
             }
         }
-        public static void getneededTime()
+        public static void getneededTime(DateTime DestinationTime)
         {
-
+            getDuration(getConnectionInfo(DestinationTime));
         }
-        public static int parseNeededTime(String theOutput)
+        private static int getDuration(string xml)
         {
-            return (0);
+            Boolean theLock = true;
+            string status = "", duration = "";
+            string[] lines = xml.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None);
+            foreach (string line in lines)
+            {
+                if (line.Contains("status"))
+                {
+                    status = extractValue(line);
+
+                }
+                else if (line.Contains("value") && theLock)
+                {
+                    duration = extractValue(line);
+                    theLock = false;
+                }
+            }
+            if (status == "OK")
+            {
+                return int.Parse(duration);
+            }
+            return -1;
         }
-        #region --Sonstige Metoden (Protected)--
+        private static String extractValue(string line)
+        {
+            String Value = "";
+            int phase = 0;
+            char[] charArray = line.ToCharArray();
+            foreach (char didgit in charArray)
+            {
+                if (phase == 0 && didgit == '>')
+                {
+                    phase++;
+                }
+                else if (phase == 1 && didgit != '<')
+                {
+                    Value += didgit;
+                }
+                else if (didgit == '<' && phase == 1)
+                {
+                    phase++;
+                }
+            }
+            return Value;
+        }
+            #region --Sonstige Metoden (Protected)--
 
 
 
-        #endregion
-        //--------------------------------------------------------Events:---------------------------------------------------------------------\\
-        #region --Events--
+            #endregion
+            //--------------------------------------------------------Events:---------------------------------------------------------------------\\
+            #region --Events--
 
 
 
-        #endregion
-    }
+            #endregion
+        }
 }
