712b206d7e1c55aea425bed3273ef128bc99ac4c
Unknown <kilian.traub@live.de>
2016-11-13 10:49:39 +0100

    Moved Server in the right Folder

---
+++ Server/main.php
@@ -0,0 +1,7 @@
+<?php 
+$KEY=$_GET["key"];
+$pTokn=$_GET["pToken"];
+print('python3.5 main.py "'+$KEY+'&pToken='+$pTokn+'"');
+//exec ('python3.5 main.py "'+$KEY+'&pToken='+$pTokn+'"');
+//readfile("export.fkt");
+?>
